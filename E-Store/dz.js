7d8e262df2761bc1f0c5a546c05da3087994f790
Boris Bovkunovich <yakov.antipov.94@mail.ru>
2025-04-21 22:22:18 +0300

    Add files via upload

---
+++ E-Store/dz.js
@@ -0,0 +1,15 @@
+// function printMultiples(k, n) {
+//  for (let i = 1; i <= n; i++) {
+//    if (i % k === 0) {
+//    }
+//  }
+//  console.log(printMultiples(k, n));
+//}
+
+function printSquaresUpToN(N) {
+  let i = 1;
+  for (let i = 1; i * i <= N; i++) {
+    result;
+  }
+  console.log(printSquaresUpToN(N));
+}
